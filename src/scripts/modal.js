649a1e328fb459c5d09639423209a89f96515f41
JhowsDev <jonathansousaandrade316@gmail.com>
2023-12-16 12:54:32 -0300

    "Tarefa 4 concluída"

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,1 +1,22 @@
 /* Desenvolva seu código aqui... */
+function creatAcount() {
+    modalControler = document.querySelector('.modal__creat-acount')
+    button = document.querySelector('.form__register-link')
+
+    button.addEventListener('click', (event) => {
+        event.preventDefault()
+        modalControler.showModal()
+        closeModal()
+    })
+}
+
+function closeModal() {
+    modalControler = document.querySelector('.modal__creat-acount')
+    button = document.querySelector('.modal__button-close')
+
+    button.addEventListener('click', () => {
+        modalControler.close()
+    })
+}
+
+creatAcount()
